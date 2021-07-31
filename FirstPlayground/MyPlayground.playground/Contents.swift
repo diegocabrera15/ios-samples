b2589891b4dedd3483ca911ff71400b9da8da298
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-07-30 23:33:24 -0500

    String review

--- FirstPlayground/MyPlayground.playground/Contents.swift
+++ FirstPlayground/MyPlayground.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, diego"
+//String in one line
+var myString = "Hello, diego"
+
+//String in multiples lines
+var myMultLines = """
+Hola Mami
+Hola Sandra
+Hola Caddy
+"""
+
+//String in multiples lines
+var myMultLines2 = """
+Hola Mami\
+Hola Sandra\
+Hola Caddy
+"""
+
+//String empty
+let myStringEmpty = ""
+let myStringEmpty2 = String()
+
