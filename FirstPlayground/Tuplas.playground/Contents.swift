e33e593ac65337b323c7aa16957075b5dbe36390
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-07-31 01:04:47 -0500

    Uses of tuples

---
+++ FirstPlayground/Tuplas.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+//First method
+var person = ("Diego", "Cabrera", 32, 1.69)
+
+person.3
+
+var (name, surname, age, stature) = person
+
+name
+
+
+//Second method
+
+var person2 = (name1: "Sand", surname1: "Tul", age1: "37", stature1: 1.65)
+
+person2.name1
