78bbc0af08d2cb8f627f024038c842f9c58a1252
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-08-04 00:17:04 -0500

    Optional binding

---
+++ FirstPlayground/OptionalBinding.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+//Enlaces opcionales
+var myOptionalString1: String?="DF"
+var myOptionalString2: String?="DF"
+var myOptionalString3: String?="DF"
+
+//Asi se hace el encadenamiento de enlaces opcionales
+if let myString = myOptionalString1, let myString2 = myOptionalString2, let myString3 = myOptionalString3 {
+	print("\(myString) \(myString2) \(myString3)")
+}else{
+	print("Alguna variable es nula")
+}
+
+//Desempaquetado frozado
+print(myOptionalString1!)
