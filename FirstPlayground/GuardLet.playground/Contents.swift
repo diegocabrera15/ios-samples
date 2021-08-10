b30f6ac45156e5d1bba30f236db80529f7fb2b48
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-08-09 22:21:32 -0500

    Guard let

---
+++ FirstPlayground/GuardLet.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+//Salida rápida guard let
+
+var myOptionalString: String?
+
+func myFunction(){
+	guard let myString = myOptionalString else {
+		print("myString es nulo")
+		return
+	}
+	print("El valor de myString es \(myString)")
+}
+
+myFunction()
+
+myOptionalString = "Text me"
+
+myFunction()
