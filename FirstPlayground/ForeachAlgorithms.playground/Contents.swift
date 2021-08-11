7329d7383d237e4539345d729662eee5bbbf9fa0
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-08-10 23:02:27 -0500

    Uses of forEach Method

--- FirstPlayground/ForeachAlgorithms.playground/Contents.swift
+++ FirstPlayground/ForeachAlgorithms.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+var myArray = [4, 3, 5, 6, 0, 9, 1]
+
+let myDictionary = [4: "Cuatro",
+					3: "Tres",
+					5: "Cinco",
+					6: "Seis",
+					0: "Cero",
+					9: "Nueve",
+					1: "Uno"
+					]
+
+let mySet: Set = [4, 3, 5, 6, 0, 9, 1]
+
+print("Iteration Array with forEach")
+myArray.forEach{(myItem) in
+	print(myItem)
+}
+print("Iteration Dictionary with forEach")
+myDictionary.forEach{(myItem) in
+	print(myItem.key)
+}
+print("Iteration Set with forEach")
+mySet.forEach{(myItem) in
+	print(myItem)
+}
