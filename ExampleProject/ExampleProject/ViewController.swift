8ac533a5f54e0b72c09a8022d076e0c11b9b0f31
Diego Cabrera <diego.cabrera.15@outlook.com>
2021-08-10 23:22:18 -0500

    New project added

---
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by MAC 1 PMO on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
